b010df78d87b2284f2e2a4a2050441651c4a280d
Ve1okot <85491311+Ve1okot@users.noreply.github.com>
2022-01-11 16:52:40 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let str = Math.abs(n).toString();
+  let reversed = '';
+  for (i = 0; i < str.length; i++) {
+    reversed = `${str[i]}${reversed}`
+  } 
+  return reversed	
 }
